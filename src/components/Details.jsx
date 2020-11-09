c19d2bef5518ea8489c61b387a23c73701dca7fd
eris <receperis@gmail.com>
2020-11-09 04:04:30 +0100

    table format established

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 
 function Details({ match, data }) {
@@ -32,24 +32,24 @@ function Details({ match, data }) {
 
     return (
 
-        <ol>
-            {id}
-            {eta}
-            {last_updated}
-            {location_coordinate_latitude}
-            {location_coordinate_longitude}
-            {location_id}
-            {location_name}
-            {location_status_ok}
-            {notes}
-            {parcel_id}
-            {sender}
-            {status}
-            {user_name}
-            {user_phone}
-            {verification_required}
+        <table>
 
-        </ol>
+            <tr> <td> Order ID   </td>      <td> {id} </td></tr>
+            <tr> <td> eta   </td>           <td> {eta} </td></tr>
+            <tr> <td> last updated </td>    <td> {last_updated}</td></tr>
+            <tr> <td> Location Coordinate Latitude</td>  <td> {location_coordinate_latitude}</td></tr>
+            <tr> <td> location_coordinate_longitude</td> <td> {location_coordinate_longitude}</td></tr>
+            <tr> <td> location_id </td>      <td> {location_id}</td></tr>
+            <tr> <td> location_name</td>     <td>{location_name}</td></tr>
+            <tr> <td> notes </td>            <td>{notes}</td></tr>
+            <tr> <td> parcel_id</td>         <td> {parcel_id}</td></tr>
+            <tr> <td> sender</td>            <td> {sender}</td></tr>
+            <tr> <td> status</td>            <td> {status}</td></tr>
+            <tr> <td> user_name </td>        <td> {user_name}</td></tr>
+            <tr> <td> user_phone</td>        <td>{user_phone}</td></tr>
+           
+
+        </table>
     )
 }
 
